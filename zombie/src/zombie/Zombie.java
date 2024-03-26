d5b53123b5cc150eb8912c3e09326dc117e31873
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-26 14:18:33 +0900

    add attack()메소드 추가

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -7,7 +7,17 @@ public class Zombie extends Unit{
 	}
 	
 	public void attack(Unit unit) {
+		int damage = ran.nextInt(9)+1;
 		
+		unit.setHp(damage);
+		
+		if(unit.getHp() <= 0) {
+			unit.setHp(0);
+			unit.setDead();
+			System.out.println(unit + "이 사망하였습니다.");
+		}else {
+			System.out.printf("%s가 %d의 공격!!\n %s", this, damage, unit);
+		}
 	}
 	
 }
