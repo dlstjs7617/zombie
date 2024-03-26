fd53c9b5def6ce693141cb4d37dff6479fd9fc2c
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-26 17:21:27 +0900

    modify attack() : bossMonster인터페이스를 추가해 난이도를 높여 유저는 앞으로 더욱 까다로운 선택을 강요받을것입니다.

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -10,13 +10,31 @@ public class Hero extends Unit{
 		int damage = ran.nextInt(20)+10;
 		System.out.println("주인공의 공격 : " + damage);
 		
-		enemy.setHp(damage);
-		
-		if(enemy.getHp() <= 0) {
-			System.out.println(enemy.getName() + "을 처치하셨습니다");
+		if(enemy instanceof bossMonster) {
+			Boss boss = (Boss)enemy;
+			
+			if(damage-boss.getShield() <= 0) {
+				enemy.setHp(0);
+			}else {
+				enemy.setHp(damage - boss.getShield());				
+			}
+			
+			if(enemy.getHp() <= 0) {
+				System.out.println(enemy.getName() + "을 처치하셨습니다");
+			}else {
+				System.out.println(enemy);
+			}
 		}else {
-			System.out.println(enemy);
+			enemy.setHp(damage);
+			
+			if(enemy.getHp() <= 0) {
+				System.out.println(enemy.getName() + "을 처치하셨습니다");
+			}else {
+				System.out.println(enemy);
+			}
 		}
+		
+		
 	}
 	
 }
