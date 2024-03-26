df9a55cfa4ee228e15093c2d37d710b32de2b487
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-26 15:00:14 +0900

    add : Boss생성자 및 shield필드값 추가 게터세터 생성

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -1,5 +1,20 @@
 package zombie;
 
 public class Boss extends Zombie{
+	private int shield;
+	
+	public Boss(String name, int hp, int shield) {
+		super("보스좀비", 400);
+		this.shield = shield;
+	}
+	
+	public void setShield(int shield) {
+		this.shield = shield;
+	}
+	
+	public int getShield() {
+		return this.shield;
+	}
+	
 	
 }
