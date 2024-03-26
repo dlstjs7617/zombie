5002f816fd62054a8f72c7d09788f25744a7483f
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-26 15:19:29 +0900

    add attack() : 보스전용 치명타공격및 공격 메소드 추가

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -16,5 +16,32 @@ public class Boss extends Zombie{
 		return this.shield;
 	}
 	
+	public void attack(Unit charater) {
+		int damage = ran.nextInt(10)+5;
+		
+		int critical = ran.nextInt(4)+1;
+		
+		if(critical == 1) {
+			int power = damage*2;
+			System.out.println("보스의 치명타!공격 :" + power);
+			
+			charater.setHp(power);
+			
+			if(charater.getHp() <= 0) {
+				System.out.println(charater.getName() +"이 " + getName() +"에 의해 사망하샸습니다");
+			}else {
+				System.out.println(charater);
+			}
+		}else {
+			System.out.println("보스의 공격 : " + damage);
+			charater.setHp(damage);
+			
+			if(charater.getHp() <= 0) {
+				System.out.println(charater.getName() +"이 " + getName() +"에 의해 사망하샸습니다");
+			}else {
+				System.out.println(charater);
+			}
+		}
+	}
 	
 }
