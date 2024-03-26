2eb92c19500c44001c460143004594a35a6156e0
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-26 17:41:48 +0900

    add bossAttack() ,nomalAttack() : attack() 메소드의 분리 메소드

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -6,32 +6,40 @@ public class Hero extends Unit{
 		super(name, hp);
 	}
 	
+	private void bossAttack(int damage, Unit enemy) {
+		Boss boss = (Boss)enemy;
+		
+		if(damage-boss.getShield() <= 0) {
+			enemy.setHp(0);
+		}else {
+			enemy.setHp(damage - boss.getShield());				
+		}
+		
+		if(enemy.getHp() <= 0) {
+			System.out.println(enemy.getName() + "을 처치하셨습니다");
+		}else {
+			System.out.println(enemy);
+		}
+	}
+	
+	private void nomalAttack(int damage, Unit enemy) {
+		enemy.setHp(damage);
+		
+		if(enemy.getHp() <= 0) {
+			System.out.println(enemy.getName() + "을 처치하셨습니다");
+		}else {
+			System.out.println(enemy);
+		}
+	}
+	
 	public void attack(Unit enemy) {
 		int damage = ran.nextInt(30)+10;
 		System.out.println("주인공의 공격 : " + damage);
 		
 		if(enemy instanceof bossMonster) {
-			Boss boss = (Boss)enemy;
-			
-			if(damage-boss.getShield() <= 0) {
-				enemy.setHp(0);
-			}else {
-				enemy.setHp(damage - boss.getShield());				
-			}
-			
-			if(enemy.getHp() <= 0) {
-				System.out.println(enemy.getName() + "을 처치하셨습니다");
-			}else {
-				System.out.println(enemy);
-			}
+			bossAttack(damage, enemy);
 		}else {
-			enemy.setHp(damage);
-			
-			if(enemy.getHp() <= 0) {
-				System.out.println(enemy.getName() + "을 처치하셨습니다");
-			}else {
-				System.out.println(enemy);
-			}
+			nomalAttack(damage, enemy);
 		}
 		
 		
