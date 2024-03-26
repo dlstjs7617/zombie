dc6752d2f21c0a0a3b6de8c729dab4275f8686ca
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-26 15:48:58 +0900

    add attack() : charater의 공격 메소드 추가

--- zombie/src/zombie/Charater.java
+++ zombie/src/zombie/Charater.java
@@ -7,10 +7,16 @@ public class Charater extends Unit{
 	}
 	
 	public void attack(Unit enemy) {
+		int damage = ran.nextInt(20)+10;
+		System.out.println("주인공의 공격 : " + damage);
 		
+		enemy.setHp(damage);
 		
-		
-		
+		if(enemy.getHp() <= 0) {
+			System.out.println(enemy.getName() + "을 처치하셨습니다");
+		}else {
+			System.out.println(enemy);
+		}
 	}
 	
 }
