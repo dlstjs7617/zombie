a61d7663b21f2935bf30ad5d36d64b7ff954c66e
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-26 16:06:07 +0900

    add Game()생성자 생성 createZombie() 메소드생성 createBoss() 메소드 생성

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -1,8 +1,9 @@
 package zombie;
 
 public class Game {
+	
 	private Game() {
-		
+		Hero hero = new Hero("주인공", 400);
 	}
 	
 	private static Game instance = new Game();
@@ -10,6 +11,15 @@ public class Game {
 	public static Game getInstance() {
 		return instance;
 	}
+	
+	private void createZombie() {
+		Zombie zombie = new Zombie("좀비", 100);
+	}
+	
+	private void createBoss() {
+		Boss boss = new Boss("보스좀비", 500, 10);
+	}
+	
 	
 	public void run() {
 		
