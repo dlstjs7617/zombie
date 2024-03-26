a570c3f98b25e6403dcf4b5c3ab9ff13eba24d1e
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-26 17:37:38 +0900

    add criticalAttack() : 치명타공격 메소드 분리

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -1,6 +1,8 @@
 package zombie;
 
 public class Boss extends Zombie implements bossMonster{
+	
+	private final int CRITICAL = 1;
 	private int shield;
 	
 	public Boss(String name, int hp, int shield) {
@@ -16,22 +18,26 @@ public class Boss extends Zombie implements bossMonster{
 		return this.shield;
 	}
 	
+	private void criticalAttack(int damage, Unit charater) {
+		int power = damage*2;
+		System.out.println("보스의 치명타!공격 :" + power);
+		
+		charater.setHp(power);
+		
+		if(charater.getHp() <= 0) {
+			System.out.println(charater.getName() +"이 " + getName() +"에 의해 사망하샸습니다");
+		}else {
+			System.out.println(charater);
+		}
+	}
+	
 	public void attack(Unit charater) {
 		int damage = ran.nextInt(10)+5;
 		
 		int critical = ran.nextInt(4)+1;
 		
-		if(critical == 1) {
-			int power = damage*2;
-			System.out.println("보스의 치명타!공격 :" + power);
-			
-			charater.setHp(power);
-			
-			if(charater.getHp() <= 0) {
-				System.out.println(charater.getName() +"이 " + getName() +"에 의해 사망하샸습니다");
-			}else {
-				System.out.println(charater);
-			}
+		if(critical == CRITICAL) {
+			criticalAttack(damage, charater);
 		}else {
 			System.out.println("보스의 공격 : " + damage);
 			charater.setHp(damage);
